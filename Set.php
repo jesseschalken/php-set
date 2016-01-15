27d6d802552798812e141d36b70e92fbe63e05bf
Jesse Schalken <me@jesseschalken.com>
2016-01-16 01:00:36 +1100

    Add Set.php, SetTest.php and Composer/PHPUnit/IDEA/Git config

---
+++ Set.php
@@ -0,0 +1,319 @@
+<?php
+
+namespace JesseSchalken;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Copy of java.utils.Set implementing ArrayAccess, Countable and IteratorAggregate with O(1) conversion to/from array
+ * keys. Only integers and strings (keys of an array) are supported.
+ *
+ * ArrayAccess is implemented as follows:
+ *
+ * - `$set[$value]`          is the same as   `$set->contains($value)`
+ * - `$set[$value] = true`   is the same as   `$set->add($value)`
+ * - `$set[$value] = false`  is the same as   `$set->remove($value)`
+ * - `isset($set[$value])`   is the same an   _error_
+ * - `unset($set[$value])`   is the same an   _error_
+ *
+ * IteratorAggregate::getIterator() returns an iterator producing the elements as values and 0,1,2...n as keys.
+ *
+ * @link https://docs.oracle.com/javase/7/docs/api/java/util/Set.html
+ */
+class Set implements ArrayAccess, Countable, IteratorAggregate {
+    /**
+     * _O(1)_
+     *
+     * Create a set from the keys of an array.
+     *
+     * Note that `$array === Set::fromArrayKeys($array)->toArrayKeys()`.
+     * @param array $array
+     * @return Set
+     */
+    public static function fromArrayKeys(array $array) {
+        $set      = new self();
+        $set->set = $array;
+        return $set;
+    }
+
+    /**
+     * @param array|Traversable $t
+     * @return array
+     */
+    private static function toKeys($t) {
+        if ($t instanceof self) {
+            return $t->set;
+        } else if (is_array($t)) {
+            return array_fill_keys($t, true);
+        } else {
+            $s = array();
+            foreach ($t as $v) {
+                $s[$v] = true;
+            }
+            return $s;
+        }
+    }
+
+    /** @var array */
+    private $set;
+
+    /**
+     * _O(n)_
+     *
+     * Create a set.
+     * @param array|Traversable $contents
+     */
+    public function __construct($contents = array()) {
+        $this->set = self::toKeys($contents);
+    }
+
+    #region java.util.Set
+
+    /**
+     * _O(1)_
+     *
+     * Adds the specified value to the set, if not already present.
+     * @param int|string $e
+     * @return void
+     */
+    public function add($e) {
+        $this[$e] = true;
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Adds all the values in the specified array or Traversable to the set, if not already present.
+     * @param array|Traversable $c
+     * @return void
+     */
+    public function addAll($c) {
+        $this->set = array_replace($this->set, self::toKeys($c));
+    }
+
+    /**
+     * _O(1)_
+     *
+     * Removes all of the values from the set.
+     * @return void
+     */
+    public function clear() {
+        $this->set = array();
+    }
+
+    /**
+     * _O(1)_
+     *
+     * Returns true if this set contains the specified value.
+     * @param int|string $e
+     * @return bool
+     */
+    public function contains($e) {
+        return $this[$e];
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Set union. Returns true if this set contains all of the values of the specified array or Traversable.
+     * @param array|Traversable $c
+     * @return bool
+     */
+    public function containsAll($c) {
+        return !array_diff_key(self::toKeys($c), $this->set);
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Returns true if this set contains exactly the values of the specified array or Traversable.
+     * @param array|Traversable $c
+     * @return bool
+     */
+    public function equals($c) {
+        $c = self::toKeys($c);
+        return
+            !array_diff_key($this->set, $c) &&
+            !array_diff_key($c, $this->set);
+    }
+
+    /**
+     * _O(1)_
+     *
+     * Return true if this set is empty.
+     * @return bool
+     */
+    public function isEmpty() {
+        return !$this->set;
+    }
+
+    /**
+     * Returns an iterator producing the elements as values and integers 0,1,2...n as keys.
+     * @return \Iterator
+     */
+    public function iterator() {
+        return $this->getIterator();
+    }
+
+    /**
+     * _O(1)_
+     *
+     * Removes the specified value from the set, if present.
+     * @param int|string $e
+     * @return void
+     */
+    public function remove($e) {
+        $this[$e] = false;
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Set difference. Removes all of the values from the specified array or Traversable from this set, if present.
+     * @param array|Traversable $c
+     * @return void
+     */
+    public function removeAll($c) {
+        $this->set = array_diff_key($this->set, self::toKeys($c));
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Set intersection. Removes all of the values *not* in the specified array or Traversable from this set.
+     * @param array|Traversable $c
+     * @return void
+     */
+    public function retainAll($c) {
+        $this->set = array_intersect_key($this->set, self::toKeys($c));
+    }
+
+    /**
+     * _O(1)_
+     *
+     * Returns the number of values in this set.
+     * @return int
+     */
+    public function size() {
+        return $this->count();
+    }
+
+    /**
+     * _O(n)_
+     *
+     * Returns an array containing the elements of this set as values.
+     * @return array
+     */
+    public function toArray() {
+        return array_keys($this->set);
+    }
+
+    #endregion
+
+    /**
+     * _O(1)_
+     *
+     * Returns an array containing the elements of this set as keys.
+     * The value may be anything (even null) and you should *not* depend on what is used as the value.
+     * @return array
+     */
+    public function toArrayKeys() {
+        return $this->set;
+    }
+
+    #region ArrayAccess
+
+    /**
+     * _O(1)_
+     *
+     * @param int|string $key
+     * @return bool
+     */
+    public function offsetGet($key) {
+        return array_key_exists($key, $this->set);
+    }
+
+    /**
+     * _O(1)_
+     *
+     * @param int|string $key
+     * @param bool       $value
+     * @return void
+     */
+    public function offsetSet($key, $value) {
+        if ($value) {
+            $this->set[$key] = true;
+        } else {
+            unset($this->set[$key]);
+        }
+    }
+
+    /**
+     * @param mixed $key
+     * @return bool|void
+     * @throws \BadMethodCallException
+     * @deprecated
+     */
+    public function offsetExists($key) {
+        throw new \BadMethodCallException(__METHOD__ . ' is not supported');
+    }
+
+    /**
+     * @param mixed $key
+     * @throws \BadMethodCallException
+     * @deprecated
+     */
+    public function offsetUnset($key) {
+        throw new \BadMethodCallException(__METHOD__ . ' is not supported');
+    }
+
+    #endregion
+
+    #region IteratorAggregate
+
+    public function getIterator() {
+        return new ArrayKeyIterator($this->set);
+    }
+
+    #endregion
+
+    #region Countable
+
+    /**
+     * _O(1)_
+     *
+     * Returns the number of values in the set.
+     * @return int
+     */
+    public function count() {
+        return count($this->set);
+    }
+
+    #endregion
+}
+
+class ArrayKeyIterator extends \ArrayIterator {
+    private $i = 0;
+
+    public function current() {
+        return parent::key();
+    }
+
+    public function next() {
+        parent::next();
+        $this->i++;
+    }
+
+    public function key() {
+        return $this->i;
+    }
+
+    public function rewind() {
+        parent::rewind();
+        $this->i = 0;
+    }
+}
+
---
+++ SetTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace JesseSchalken;
+
+class SetTest extends \PHPUnit_Framework_TestCase {
+    public function testCreate() {
+        $set = new Set(['a', 'b']);
+        $this->assertTrue($set->contains('a'));
+        $this->assertTrue($set->contains('b'));
+        $this->assertFalse($set->contains('c'));
+        $this->assertEquals(['a', 'b'], $set->toArray());
+    }
+
+    public function testCreateTraversable() {
+        $set = new Set(new \ArrayIterator([10, 11]));
+        $this->assertTrue($set->containsAll([11, 10]));
+        $this->assertFalse($set->containsAll([-9, 11, 10]));
+    }
+
+    public function testCreateCopy() {
+        $set1 = new Set();
+        $set1->add('an string');
+        $set1->add('100');
+
+        // Copy via constructor
+        $set2 = new Set($set1);
+        $this->assertEquals($set2->toArray(), ['an string', 100]);
+
+        // Copy using to/from array keys
+        $set3 = Set::fromArrayKeys($set2->toArrayKeys());
+        $this->assertEquals($set3->toArray(), ['an string', 100]);
+
+        // Just make sure they're not the same object before testing equals()
+        $this->assertFalse($set1 === $set2);
+        $this->assertFalse($set2 === $set3);
+
+        $this->assertTrue($set1->equals($set2));
+        $this->assertTrue($set2->equals($set3));
+        $this->assertFalse($set1->equals([]));
+        $this->assertFalse($set1->equals(['lol']));
+    }
+
+    public function testAddAll() {
+        $set1 = new Set(['lol']);
+        $set2 = new Set(['foo']);
+        $set1->addAll($set2);
+        $set2->addAll($set1);
+        $this->assertTrue($set1->equals($set2));
+    }
+
+    public function testClear() {
+        $set1 = new Set([1, 6, 4, 3]);
+        $this->assertEquals($set1->size(), 4);
+        $set1->clear();
+        $this->assertTrue($set1->isEmpty());
+        $this->assertEquals($set1->size(), 0);
+        $set1->add('foo');
+        $this->assertEquals($set1->count(), 1);
+    }
+
+    public function testIterate() {
+        $array = [
+            'foo',
+            'bar',
+            'baz',
+            9000,
+        ];
+
+        $set = new Set($array);
+        $this->assertEquals(iterator_to_array($set), $array);
+        $this->assertEquals(iterator_to_array($set->iterator()), $array);
+    }
+
+    public function testArrayAcces() {
+        $set          = new Set;
+        $set[0]       = true;
+        $set[-900]    = true;
+        $set[0]       = false;
+        $set['set']   = true;
+        $set['noset'] = false;
+        $this->assertEquals($set->toArray(), [-900, 'set']);
+    }
+
+    public function testRemove() {
+        $set1 = new Set();
+        $set2 = new Set([100, 200]);
+        $set1->add(200);
+        $set1->add(300);
+        $set2->retainAll($set1);
+        $this->assertEquals($set1->toArray(), [200, 300]);
+        $this->assertEquals($set2->toArray(), [200]);
+        $this->assertFalse($set1->equals($set2));
+        $this->assertFalse($set2->equals($set1));
+        $set1->remove(300);
+        $this->assertTrue($set1->equals($set2));
+        $this->assertTrue($set2->equals($set1));
+        $set1->removeAll($set2);
+        $this->assertTrue($set1->isEmpty());
+        $this->assertFalse($set2->isEmpty());
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     * @expectedExceptionMessage JesseSchalken\Set::offsetUnset is not supported
+     * @expectedExceptionCode  0
+     */
+    public function testArrayOffsetUnset() {
+        $set = new Set;
+        unset($set[0]);
+    }
+
+    /**
+     * @expectedException \BadMethodCallException
+     * @expectedExceptionMessage JesseSchalken\Set::offsetExists is not supported
+     * @expectedExceptionCode  0
+     */
+    public function testArrayOffsetIsset() {
+        $set = new Set;
+        print isset($set[0]);
+    }
+}
